00d5688911ef84342c953e245320a7d04cb51074
Mohamed-Khalil-Dhaher <dhaherkhalil91@gmail.com>
2020-12-29 14:58:51 +0100

    login with google

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { APP_BASE_HREF } from '@angular/common';
 import { EventComponent } from './event/event.component';
 import { AppointmentComponent } from './appointment/appointment.component';
 import { AppointmentListComponent } from './appointment-list/appointment-list.component';
-import { MatTableModule } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -23,23 +23,67 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatNativeDateModule } from '@angular/material/core';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
-
-
+import {
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+} from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+} from 'angularx-social-login';
 
 @NgModule({
-  declarations: [SignupComponent,LoginComponent,UserComponent,HomeComponent,AppComponent, AddBlogComponent, DisplayBlogsComponent, EventComponent, AppointmentComponent, AppointmentListComponent, LoginComponent,
+  declarations: [
     SignupComponent,
-    routingComponents],
-  imports: [HttpClientModule,BrowserModule, AppRoutingModule, FormsModule,MatTableModule,
+    LoginComponent,
+    UserComponent,
+    HomeComponent,
+    AppComponent,
+    AddBlogComponent,
+    DisplayBlogsComponent,
+    EventComponent,
+    AppointmentComponent,
+    AppointmentListComponent,
+    LoginComponent,
+    SignupComponent,
+    routingComponents,
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatTableModule,
     MatProgressSpinnerModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatToolbarModule],
-    providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+    MatToolbarModule,
+    SocialLoginModule,
+  ],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '620902958049-iv3dlhu14iengn0baj4prumpfkie3rk5.apps.googleusercontent.com'
+            ),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('clientId'),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
+  ],
   bootstrap: [AppComponent],
-
 })
 export class AppModule {}
